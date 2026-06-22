b358641dbaad824741f8a6464d3c30d94d0bc56b
Lucas Holder <lucas.j.holder@gmail.com>
2026-06-22 08:39:23 -0700

    Made formatting consistent.

--- cropcsvtojson.py
+++ cropcsvtojson.py
@@ -1,34 +1,37 @@
 import csv
 import json
-jsondict = {}
+jsonDict = {}
 
-with open('cropcsv.csv', newline="") as csvfile:
-    cropreader = csv.reader(csvfile)
-    for row in cropreader:
+with open('cropcsv.csv', newline="") as csvFile:
+    cropReader = csv.reader(csvFile)
+    for row in cropReader:
         stripped_row = []
         for item in row:
             stripped_row.append(item.strip())
         if stripped_row[0] == "Tier": continue
         tier = stripped_row[0]
-        friendlyname = stripped_row[1]
+        friendlyName = stripped_row[1]
         name = stripped_row[1].lower().replace(" ", "")
         soil = stripped_row[2]
-        seedbed = stripped_row[3]
-        growthpoints = int(stripped_row[4])
-        biomepreferences = stripped_row[5].split(", ")
+        seedBed = stripped_row[3]
+        growthPoints = int(stripped_row[4])
+        biomePreferences = stripped_row[5].split(", ")
         requirements = stripped_row[6]
-        dropmult = float(stripped_row[7].replace("x", ""))
-        outputs = stripped_row[8]
-        jsondict[name] = {"tier": tier,
-                          "friendlyname": friendlyname,
+        dropMult = float(stripped_row[7].replace("x", ""))
+        outputs = stripped_row[8].split(", ")
+        dissectedOutputsArray = {}
+        for output in outputs:
+
+        jsonDict[name] = {"tier": tier,
+                          "friendlyname": friendlyName,
                           "name": name,
                           "soil": soil,
-                          "seedbed": seedbed,
-                          "growthpoints": growthpoints,
-                          "biomepreferences": biomepreferences,
+                          "seedbed": seedBed,
+                          "growthpoints": growthPoints,
+                          "biomepreferences": biomePreferences,
                           "requirements": requirements,
-                          "dropmult": dropmult,
+                          "dropmult": dropMult,
                           "outputs": outputs}
-print(jsondict)
+print(jsonDict)
 with open("cropjson.json", "w") as jsonfile:
-    json.dump(jsondict,jsonfile)
+    json.dump(jsonDict,jsonfile)
